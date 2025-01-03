3493bea68d4fdff96e3b6bddb53edf96639d931b
The Capital Hub <141011134+the-capital-hub@users.noreply.github.com>
2025-01-03 19:09:55 +0530

    Update index.ts
    removed db console

--- index.ts
+++ index.ts
@@ -31,7 +31,6 @@ const startServer = async () => {
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}/graphql`);
-    console.log('DATABASE_URL:', process.env.DATABASE_URL);
   });
 };
 
