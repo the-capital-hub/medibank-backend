b65883efbcb03afc072ba142afa069f49d43fe7b
Vishal Mishra <vm174294@gmail.com>
2025-02-20 14:39:21 +0530

    updated get api and created delete api.

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (payload: object): string =>
-  jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: "1h" });
+  jwt.sign(payload, process.env.JWT_SECRET!);
 
 export const verifyToken = (token: string): { userId: string } =>
   jwt.verify(token, process.env.JWT_SECRET!) as { userId: string };
